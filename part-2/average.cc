2504c8de9622ed8ee758929a734b0faad798fc7a
Issa <sofiaissa0104@cs300u-5b.acad.fullerton.edu>
2023-11-13 17:04:54 -0800

    done

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,20 +1,20 @@
 // Sofia Issa
 // sofiaissa0104@csu.fullerton.edu
 // @sofiaissa0104
-// Partners: @HeroesMEX @alexistakeuchi
+// Partners: @HeroesMEX, @alexistakeuchi
 
 #include <iostream>
 #include <string>
 #include <vector>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
   if (arguments.size() <= 1) {
     std::cout << "Error: you must supply at least one number";
     return 1;
   }
   double sum = 0.0;
-  for (std::string const& argument : arguments) {
+  for (std::string const &argument : arguments) {
     if (argument == arguments[0]) {
       continue;
     }
